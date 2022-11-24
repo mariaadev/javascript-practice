07a67f9b7e70369d85ac8823afe4b3c8a6b81d67
Maria Amzil <mini.mar@hotmail.com>
2022-11-24 18:08:42 +0100

    Practice Javascript starting point

---
+++ challenge.js
@@ -0,0 +1,17 @@
+
+const students = [{
+    age: 32,
+    examScores: [],
+    gender: 'male',
+    name: 'edu'
+  },
+  {
+    age: 29,
+    examScores: [],
+    gender: 'female',
+    name: 'silvia'
+  }]
+  
+  const availableMaleNames = ['pepe', 'juan', 'victor', 'Leo', 'francisco', 'carlos'];
+  const availableFemaleNames = ['cecilia', 'ana', 'luisa', 'silvia', 'isabel', 'virginia'];
+  const availableGenders = ['male', 'female'];
