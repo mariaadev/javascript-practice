dd125e20bb0e6dc17bfc15dc54c8225eb9486d0c
Maria Amzil <mini.mar@hotmail.com>
2022-11-24 21:53:41 +0100

    Add requirements from 1 to 7 included

--- challenge.js
+++ challenge.js
@@ -57,7 +57,7 @@ async function manageStudents() {
         return new Promise((resolve, reject) => {
             rl.question('Introduce the number of your option, please: ', function (option) {
                 rl.pause();
-                const parsedOption = parseInt(option)
+                const parsedOption = parseInt(option);
                 resolve(parsedOption);
             })
         });
@@ -69,14 +69,102 @@ async function manageStudents() {
 
         //Out of the loop if invalid input
         if (userOption <= 0 || userOption > 15) {
-            isInvalidInput = true
+            isInvalidInput = true;
             break;
+        } else {
+            fetchOutput(userOption);
         }
     }
     
+    
+    //Option 3
+    function nameStudents () {
+        for (let student of students) {
+            student.name;
+        }
+    } 
+
+    //Option 5
+    function removeRandomItem() {
+        //Don't add + 1 because array starts at 0
+        return students.splice(Math.floor(Math.random * students.length ),1);
+    }
+
+    //Option 6
+
+    function filterGirls () {
+        return students.filter( student => (
+            student.gender === "female"
+       ) )
+    } 
+
+    //Option 7
+
+    function filterBoys () {
+        return students.filter(student => (
+            student.gender === "male"
+        ))
+    }
+    
+    function fetchOutput(input) {
+    
+        let output;
+        
+        switch (input) {
+            case 1:
+                output = console.table(students);
+                break;
+            case 2:
+                output = console.log(students.length);
+                break;
+            case 3:
+                output = nameStudents();
+                break;
+            case 4:
+                output = console.log(students.pop());
+                break;
+            case 5:
+                output = console.log(removeRandomItem());
+                break;
+            case 6:
+                console.log(filterGirls());
+                break;
+            case 7:
+                console.log(`Girls: ${filterGirls().length}\nBoys: ${filterBoys().length}`);
+                break;
+            case 8:
+                console.log('Oranges are $0.59 a pound.');
+                break;
+            case 9:
+                console.log('Mangoes and papayas are $2.79 a pound.');
+                break;
+            case 10:
+                console.log('Mangoes and papayas are $2.79 a pound.');
+                break;
+            case 11:
+                console.log('Oranges are $0.59 a pound.');
+                break;
+            case 12:
+                console.log('Mangoes and papayas are $2.79 a pound.');
+                break;
+            case 13:
+                console.log('Mangoes and papayas are $2.79 a pound.');
+                break;
+            case 14:
+                console.log('Mangoes and papayas are $2.79 a pound.');
+                break;
+            case 15:
+                console.log('Mangoes and papayas are $2.79 a pound.');
+                break;
+            default:
+                console.log(`You didn't introduce a valid number.`);
+        }
+        
+        return output;
+    }
         //Close Console Interface
     rl.close();
 
 }
 
-manageStudents()
+manageStudents();
